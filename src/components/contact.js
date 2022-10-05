6d102f7c22ce561004394ddfe674dbedf5cac630
Nockne <nguyendillon118@gmail.com>
2022-10-05 14:56:31 -0700

    styling of contact page

--- src/components/contact.js
+++ src/components/contact.js
@@ -2,7 +2,10 @@ import React from 'react'
 
 const Contact = () => {
   return (
-    <div>Contact</div>
+    <div>
+        <h1 className="text-3xl font-mono flex justify-center text-white">Contact Me</h1>
+        
+    </div>
   )
 }
 
